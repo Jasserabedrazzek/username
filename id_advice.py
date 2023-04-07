968bad28feb6c247a79ac0e40e83c9b1a5031ead
jasser ABR <118571919+Jasserabedrazzek@users.noreply.github.com>
2023-04-07 13:47:01 +0200

    Update id_advice.py

--- id_advice.py
+++ id_advice.py
@@ -1,23 +1,18 @@
 import streamlit as st
-from PIL import Image
 import pytesseract
+from PIL import Image
 
-pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'  # Replace with the path to your Tesseract executable
+st.title("Image to Text Converter")
 
-pytesseract.pytesseract.tesseract_cmd = r"/home/appuser/venv/lib/python3.9/site-packages/pytesseract/pytesseract.py" 
-# Title of the app
-st.title("Image to Text Conversion")
+# Display file uploader widget
+uploaded_file = st.file_uploader("Choose an image file", type=["jpg", "jpeg", "png"])
 
-# Add a sidebar with instructions
-st.sidebar.header("Instructions")
-st.sidebar.markdown("1. Upload an image or capture from the camera.")
-st.sidebar.markdown("2. Click the 'Convert' button to convert image to text.")
+# If user has uploaded a file
+if uploaded_file is not None:
+    # Load image from file
+    img = Image.open(uploaded_file)
+    # Convert image to text using PyTesseract
+    text = pytesseract.image_to_string(img)
+    # Display text in Streamlit app
+    st.text_area("enter some text",text)
 
-# Use st.file_uploader to allow user to upload an image
-uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
-if uploaded_file:
-    # Open and display the image
-    image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
-    text = pytesseract.image_to_string(image)
-    txt = st.text_area("Enter some text", text)
